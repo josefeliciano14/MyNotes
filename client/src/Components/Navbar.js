c1622a53d993a70598a116666a4420d478ae3c0b
josefeliciano14 <jose.feliciano14@upr.edu>
2022-12-18 12:38:40 -0400

    Implemented Navbar
    Implemented Navbar

--- client/src/Components/Navbar.js
+++ client/src/Components/Navbar.js
@@ -1,6 +1,48 @@
+import {useState,useEffect} from 'react'
+import { Link, useNavigate} from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCaretDown } from '@fortawesome/free-solid-svg-icons'
+
+import decode from 'jwt-decode';
+
+import './Navbar.css';
+
 function Navbar(){
+
+    const [user, setUser] = useState("");
+
+    const nav = useNavigate();
+
+    useEffect(() => {
+        let token = localStorage.getItem('auth');
+        
+        if(token){
+            setUser(decode(token).username);
+        }
+    }, []);
+    
+    function logout(){
+        localStorage.clear("auth");
+    
+        setUser("");
+    
+        nav("/");
+    }
+
     return(
-        <nav>This is a navbar</nav>
+        <nav>
+            <Link to='/'><span className='logo'>ToDo</span></Link>
+            <div className='emptyspace'></div>
+            <i className="fa-solid fa-bars"></i>
+            <ul className='nav-links'>
+            <li>
+                { user
+                    ? <div>{user} <FontAwesomeIcon icon={faCaretDown}/></div>
+                    : <Link to='/login'>Login</Link> 
+                }
+            </li>
+            </ul>
+        </nav>
     )
 }
 
